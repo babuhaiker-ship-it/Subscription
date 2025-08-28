c1b33d3b5c339a7171ec4b4fe66bd397f85d5009
Godfatherpy <fschool749@gmail.com>
2025-08-28 12:29:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,12 @@ from pymongo import MongoClient, ReturnDocument
 
 # --- Configuration ---
 class PaymentConfig:
-    BOT_TOKEN = 'YOUR_PAYMENT_RECEIVER_BOT_TOKEN'
+    BOT_TOKEN = '7673807124:AAETa1Bty4C4CU0De1PuP31FwMXLmgPwQLk'
     API_ID = 29800015
     API_HASH = 'c8f37108be31ab9ea2818bfe533fbb6f'
     MONGO_URI = 'mongodb+srv://Pyasipriya:00pEcao9sYhNC5VQ@cluster0.2dfenf7.mongodb.net/spicybot?retryWrites=true&w=majority&appName=Cluster0'
     MONGO_DB_NAME = 'spicybot'
-    PAYMENT_GROUP_ID = -1001234567890  # Replace with your private SMS forwarding group ID
+    PAYMENT_GROUP_ID = -1002685844988  # Replace with your private SMS forwarding group ID
     PREMIUM_PRICE_INR = 199
     PREMIUM_DURATION_DAYS = 30
     YOUR_UPI_ID = "your-upi-id@oksbi" # Replace with your actual UPI ID
