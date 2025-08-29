d2e6b811376c5cf77e0423d98e647e368e981bc9
Godfatherpy <fschool749@gmail.com>
2025-08-29 16:03:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class PaymentConfig:
     API_HASH = 'c8f37108be31ab9ea2818bfe533fbb6f'
     MONGO_URI = 'mongodb+srv://Pyasipriya:00pEcao9sYhNC5VQ@cluster0.2dfenf7.mongodb.net/spicybot?retryWrites=true&w=majority&appName=Cluster0'
     MONGO_DB_NAME = 'spicybot'
-    PAYMENT_GROUP_ID = -1002685844988  # Replace with your private SMS forwarding group ID
+    PAYMENT_GROUP_ID = -1002905578107  # Replace with your private SMS forwarding group ID
     PREMIUM_PRICE_INR = 199
     PREMIUM_DURATION_DAYS = 30
     YOUR_UPI_ID = "your-upi-id@oksbi" # Replace with your actual UPI ID
